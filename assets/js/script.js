54ee280a8690cd8ac970dd6f14bb1b0e190a15b3
Muffin5671 <geometrydashmeltrightdown@gmail.com>
2026-01-18 15:46:24 -0500

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function readLVLParameters() {
+  let levelName = new URLSearchParams(document.location.search).get('levelName');
+  let levelDesc = new URLSearchParams(document.location.search).get('levelDesc');
+  if (levelName = undefined) {
+    document.location = '../';
+  }
+}
