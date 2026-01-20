69fa336cf9609381e654c36e9d2d767f36f7a7fe
Muffin5671 <geometrydashmeltrightdown@gmail.com>
2026-01-20 14:03:23 -0500

    Update script.js

--- editor/js/script.js
+++ editor/js/script.js
@@ -3,20 +3,30 @@ function readLVLParameters() {
   let levelDesc = new URLSearchParams(document.location.search).get('levelDesc');
 }
 
+function updateBGCol {
+  document.getElementById('editorBG').style.backgroundColor = document.getElementById('bgColorPicker').value;
+} 
+
 function saveLevel() {
   let levelName = new URLSearchParams(document.location.search).get('levelName');
   let levelDesc = new URLSearchParams(document.location.search).get('levelDesc');
+  let lastUpdated = new Date();
   let levelData = {
     "metadata": {
       "levelName":levelName,
-      "levelDesc":levelDesc
+      "levelDesc":levelDesc,
+      "lastUpdated":lastUpdated
       },
-    "bgColor":"#ff0000",
-    "grndColor":"#f00000",
+    "bgColor":document.getElementById('editorBG').style.backgroundColor,
+    "grndColor":"rgb(0,0,0)",
     "data": [
       {"object":1,"x":15,"y":15},
       {"object":1,"x":45,"y":15}
       ]
   };
   localStorage.GDEditor = JSON.stringify(levelData);
+}
+
+function loadLevel() {
+  document.getElementById('editorBG').style.backgroundColor = JSON.parse(localStorage.GDEditor).bgColor;
 }
