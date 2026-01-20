518d6a42b8ba225950c442ea395c0b337ca066db
Muffin5671 <geometrydashmeltrightdown@gmail.com>
2026-01-20 10:54:50 -0500

    Update script.js

--- editor/js/script.js
+++ editor/js/script.js
@@ -2,3 +2,19 @@ function readLVLParameters() {
   let levelName = new URLSearchParams(document.location.search).get('levelName');
   let levelDesc = new URLSearchParams(document.location.search).get('levelDesc');
 }
+
+function saveLevel {
+  let levelData = {
+    "metadata": {
+      "levelName":levelName,
+      "levelDesc":levelDesc
+      },
+    "bgColor":"#ff0000",
+    "grndColor":"#f00000",
+    "data": [
+      {"object":1,"x":15,"y":15},
+      {"object":1,"x":45,"y":15}
+      ]
+  };
+  localStorage.GDEditor = JSON.stringify(levelData);
+}
