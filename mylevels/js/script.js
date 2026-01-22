582325490d4b968790d9255f938eb10ed1fe4bdc
Muffin5671 <geometrydashmeltrightdown@gmail.com>
2026-01-21 23:49:49 -0500

    Update script.js

--- mylevels/js/script.js
+++ mylevels/js/script.js
@@ -1,4 +1,4 @@
-function createLVLListItem() {
+window.onload = function createLVLListItem() {
   let tag = document.createElement('div');
   tag.innerHTML = '<p style="background-color: var(--gdbrown); width: 500px; margin-top: 0; margin-bottom: 0;"><span class="helvetica">' + JSON.parse(localStorage.GDEditor).metadata.levelName + '</span></p><p style="background-color: var(--gdbrown); color: #00000066; border-bottom: 1.5px #00000066 solid; width: 500px; margin-top: 0; margin-bottom: 0;"><span class="helvetica" style="font-size: 16px;">Last updated: ' + JSON.parse(localStorage.GDEditor).metadata.lastUpdated + '</span></p>';
   document.body.appendChild(tag)
