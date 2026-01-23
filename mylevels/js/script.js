87b1d954a074c64a9bd3a81007af19c9e1519ad8
Muffin5671 <geometrydashmeltrightdown@gmail.com>
2026-01-23 09:47:57 -0500

    Update script.js

--- mylevels/js/script.js
+++ mylevels/js/script.js
@@ -5,7 +5,7 @@ window.onload = function createLVLListItem() {
     document.body.appendChild(tag);
   } catch (error) {
     let tag = document.createElement('div');
-    if (JSON.parse(localStorage.GDEditorDev)) {
+    if (JSON.parse(localStorage.GDEditorDev == true)) {
       tag.innerHTML = '<p><span class="helvetica">An <cr>error</cr> occured while loading level list.</span></p><br><code>' + error + '</code>';
       document.body.appendChild(tag);
     } else {
