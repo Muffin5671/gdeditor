1f3a21fee23dff3ce9abe556221764218c7f5ced
Muffin5671 <geometrydashmeltrightdown@gmail.com>
2026-01-28 22:19:44 -0500

    Update script.js

--- editor/js/script.js
+++ editor/js/script.js
@@ -18,53 +18,23 @@ function saveLevel() {
   let levelName = new URLSearchParams(document.location.search).get('levelName');
   let levelDesc = new URLSearchParams(document.location.search).get('levelDesc');
   let lastUpdated = new Date().toDateString();
+  let bgColor = document.getElementById('editorBG').style.backgroundColor;
   let levelData = {
-    "metadata": {
-      "levelName":levelName,
-      "levelDesc":levelDesc,
-      "lastUpdated":lastUpdated
+    metadata: {
+      levelName: levelName,
+      levelDesc: levelDesc,
+      lastUpdated: lastUpdated
       },
-    "bgColor":document.getElementById('editorBG').style.backgroundColor,
-    "grndColor":"rgb(0,0,0)",
-    "data": [
-      {"object":1,"x":15,"y":15},
-      {"object":1,"x":45,"y":15}
+    bgColor: bgColor,
+    grndColor: "rgb(0,0,0)",
+    data: [
+      {object: 1, x: 15, y: 15},
+      {object: 1, x: 45, y: 15}
       ]
   };
-  localStorage.GDEditor = JSON.stringify(levelData);
+  localStorage.GDEditorLevels = JSON.stringify(levelData);
 }
 
 function loadLevel() {
-  document.getElementById('editorBG').style.backgroundColor = JSON.parse(localStorage.GDEditor).bgColor;
+  document.getElementById('editorBG').style.backgroundColor = JSON.parse(localStorage.GDEditorLevels).bgColor;
 }
-
-//function setupSaveLVLAsFile(url, filename) {
-  // and then some copy and pasted code 
-  // i do not know how to download files in js
-//  const a = document.createElement("a");
-//  a.href = url;
-//  a.download = filename || "file-name";
-//  document.body.appendChild(a);
-//  a.click();
-//  document.body.removeChild(a);
-//}
-
-// function saveLVLAsFile() {
-//  const file = new Blob([
-//    "metadata": {
-//      "levelName":levelName,
-//      "levelDesc":levelDesc,
-//      "lastUpdated":lastUpdated
-//      },
-//    "bgColor":document.getElementById('editorBG').style.backgroundColor,
-//    "grndColor":"rgb(0,0,0)",
-//    "data": [
-//      {"object":1,"x":15,"y":15},
-//      {"object":1,"x":45,"y":15}
-//      ]
-//    };
-//  ], { type: "text/plain" });
-//  const url = window.URL.createObjectURL(file);
-//  setupSaveLVLAsFile(url, JSON.parse(localStorage.GDEditor).metadata.levelName + ".json");
-//  window.URL.revokeObjectURL(url);
-//}
