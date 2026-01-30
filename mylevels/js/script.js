4f4257735f1405c802325e2c507cfeb4330b82fa
Muffin5671 <geometrydashmeltrightdown@gmail.com>
2026-01-30 11:07:36 -0500

    Update script.js

--- mylevels/js/script.js
+++ mylevels/js/script.js
@@ -1,11 +1,13 @@
+// create levels / dev stuff
+// i also never memorized how to add elements :))))
 window.onload = function createLVLListItem() {
   try {
     let tag = document.createElement('div');
-    tag.innerHTML = '<p style="background-color: var(--gdbrown); width: 500px; margin-top: 0; margin-bottom: 0;"><span class="helvetica">' + JSON.parse(localStorage.GDEditor).metadata.levelName + '</span></p><p style="background-color: var(--gdbrown); color: #00000066; border-bottom: 1.5px #00000066 solid; width: 500px; margin-top: 0; margin-bottom: 0;"><span class="helvetica" style="font-size: 16px;">Last updated: ' + JSON.parse(localStorage.GDEditor).metadata.lastUpdated + '</span></p>';
+    tag.innerHTML = '<p style="background-color: var(--gdbrown); width: 500px; margin-top: 0; margin-bottom: 0;"><span class="helvetica">' + JSON.parse(localStorage.GDEditorLevels).metadata.levelName + '</span></p><p style="background-color: var(--gdbrown); color: #00000066; border-bottom: 1.5px #00000066 solid; width: 500px; margin-top: 0; margin-bottom: 0;"><span class="helvetica" style="font-size: 16px;">Last updated: ' + JSON.parse(localStorage.GDEditor).metadata.lastUpdated + '</span></p>';
     document.body.appendChild(tag);
   } catch (error) {
     let tag = document.createElement('div');
-    if (JSON.parse(localStorage.GDEditorDev) == true) {
+    if !(JSON.parse(localStorage.GDEditorDev) == undefined) {
       tag.innerHTML = '<p><span class="helvetica">An <cr>error</cr> occured while loading level list.</span></p><br><code>' + error + '</code>';
       document.body.appendChild(tag);
     } else {
@@ -20,10 +22,11 @@ window.onload = function createLVLListItem() {
   }
 }
 
+// delete all levels function
 function clearAllLVLs() {
   if (confirm('Are you sure you want to delete all of your levels?')) {
     if (confirm('Are you REALLY sure you want to delete ALL of your created levels? This cannot be undone.')) {
-      localStorage.removeItem('GDEditor');
+      localStorage.removeItem('GDEditorLevels');
       window.location.href = 
     '/gdeditor/mylevels/?clearMessage=1';
     }
