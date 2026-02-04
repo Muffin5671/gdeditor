f8a02eb815d9a39470090e775f32436366cf4768
Muffin5671 <geometrydashmeltrightdown@gmail.com>
2026-02-04 13:37:04 -0500

    Update script.js

--- profile/js/script.js
+++ profile/js/script.js
@@ -1,12 +1,13 @@
 function userNameCheck() {
-  if (document.getElementById('userNameInput').value == '') {
+  if (document.getElementById('userNameInput').value == '' || /\s/.test(document.getElementById('userNameInput').value)) {
     alert('Username cannot be empty!');
   } else {
     if (document.getElementById('userNameInput').value == 'MuffinGDYT') {
       localStorage.GDEditorDev = true;
     } else {
-      localStorage.GDEditorDev = false;
+      localStorage.removeItem('GDEditorDev');
     }
-    window.location.replace('editor/?levelName=' + encodeURI(document.getElementById('levelNameInput').value))
+    console.log('Successfully changed username!');
+    window.location.replace('../?u=' + encodeURI(document.getElementById('userNameInput').value));
   }
 }
