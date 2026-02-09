02951a751812685da888cb20335514c732c8058c
Muffin5671 <geometrydashmeltrightdown@gmail.com>
2026-02-09 12:48:36 -0500

    Create script.js

---
+++ upload/js/script.js
@@ -0,0 +1,21 @@
+async function getFileMetadata() {
+  let file = document.getElementById('levelFileInput').files[0];
+  let fileContent = await file.text();
+  return JSON.parse(fileContent).metadata;
+}
+
+async function getFileData() {
+  let file = document.getElementById('levelFileInput').files[0];
+  let fileContent = await file.text();
+  return JSON.parse(fileContent).data;
+}
+
+function fileCheck() {
+  let levelMetadata = await getFileMetadata();
+  let levelData = await getFileData();
+  if (document.getElementById('levelFileInput').files[0] == undefined) {
+    alert('Please provide a level file.');
+  } else {
+    window.location.replace('../editor/?levelName=' + encodeURI(levelMetadata.levelName) + '&levelDesc=' + encodeURI(levelMetadata.levelDesc) + '&levelData=' + encodeURI(levelData.levelDesc));
+  }
+}
